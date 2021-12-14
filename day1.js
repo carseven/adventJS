4acdd6aaee630b7592b8ecbb894352198f073e07
carseven <carles.serra@hotmail.es>
2021-12-14 23:28:12 +0100

    Add challenge url

--- day1.js
+++ day1.js
@@ -1,3 +1,6 @@
+/**
+ * https://adventjs.dev/challenges/01
+ */
 export default function contarOvejas(ovejas) {
     const nameRegex = new RegExp('[aA][nN]')
     return ovejas.filter((it) => it.color === 'rojo' && nameRegex.test(it.name));
