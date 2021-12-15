977f7d93250fe8f19615a47a9e27eb24060e9715
carseven <carles.serra@hotmail.es>
2021-12-15 23:23:50 +0100

    Resolve challenge day 7

---
+++ day7.js
@@ -0,0 +1,11 @@
+export default function contains(store, product) {
+    if (store === product) return true;
+    if (typeof store === 'object') {
+        for (const key in store) {
+            if (contains(store[key], product)) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
