ffcbcf5d33a4a07763ee2a77324e7e1f98d0b450
carseven <carles.serra@hotmail.es>
2021-12-23 23:22:43 +0100

    Resolve challenge day 9

--- day9.js
+++ day9.js
@@ -3,5 +3,11 @@
  */
 export default function groupBy(collection, it) {
     // ¡No olvides compartir tu solución en redes!
-    return {}
+    let groupByObj = {}
+    collection.forEach(item => {
+        let index = (typeof it === 'function') ? it(item) : item[it];
+        groupByObj[index] = groupByObj[index] ?? [];
+        groupByObj[index].push(item)
+    });
+    return groupByObj;
 }
