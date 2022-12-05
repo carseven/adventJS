0b255f1707ab256c531d9944a1a81a1ea15aa33e
carseven <carles.serra@hotmail.es>
2022-12-05 22:47:46 +0100

    feat(2022): day 5 solutions

--- 2022/day5.test.ts
+++ 2022/day5.test.ts
@@ -35,36 +35,24 @@ To consider:
 * The number of maxCities can be greater than giftsCities.length
 */
 
-function getMaxGifts(
+function getMaxGifts2(
   giftsCities: number[],
   maxGifts: number,
   maxCities: number
 ): number {
-  const POSSIBLE_COMBINTAIONS = 2 ** giftsCities.length;
-
-  // Documentation: https://algotree.org/algorithms/numeric/subsets_bitwise/
-  // Original solution to find the combination: https://github.com/mamunoz-dev/adventjs-2022/blob/main/day5.md
-  const validCombinations = new Array(POSSIBLE_COMBINTAIONS)
-    .fill() // Creates an empty arrray
-    // Create each possible combination
-    .map((_, finalArrayIndex) => {
-      return giftsCities.filter((_, originalArrayIndex) => {
-        // For each index, validate if it's covered by the mask (Read the documentation)
-        const MASK = 2 ** originalArrayIndex;
-        // If the condition is true, we return the element wich is in the
-        // original array at the current index originalArrayIndex.
-        return finalArrayIndex & MASK;
-      });
+  const validCombinations = new Array(2 ** giftsCities.length)
+    .fill(undefined)
+    .map((_, index) => {
+      return giftsCities.filter(
+        (_, originalArrayIndex) => index & (2 ** originalArrayIndex)
+      );
     })
-    // Remove combinations that exceed the cities limit
     .filter((combination) => combination.length <= maxCities)
-    // Get the sum of each combination
     .map((combination) => combination.reduce((acc, curr) => acc + curr, 0))
-    // Remove combinations that exceed the gifts limit
     .filter((sum) => sum <= maxGifts)
     .sort((a, b) => a - b);
 
-  return validCombinations[validCombinations.length - 1];
+  return validCombinations.pop() || 0;
 }
 
 function getMaxGifts(giftsCities, maxGifts, maxCities) {
